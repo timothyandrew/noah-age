9be49f0358940b037dfa2cd0a7ab71e05e4b9ea9
Timothy Andrew <mail@timothyandrew.net>
2017-10-21 22:32:23 +0530

    initial commit

---
+++ app.rb
@@ -0,0 +1,18 @@
+require 'rubygems'
+require 'bundler/setup'
+
+require 'sinatra'
+require 'jekyll-timeago'
+
+
+configure { set :server, :puma }
+
+BIRTH = DateTime.parse('2017-10-12T12:06:00+05:30')
+
+def since_birth
+  Jekyll::Timeago::Core.timeago(BIRTH, DateTime.now, depth: 4)
+end
+
+get '/' do
+  "Noah Andrew was born #{since_birth}."
+end
