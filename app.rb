62b0f1de6b8dbf5769a3d5f557938726dab993de
Timothy Andrew <mail@timothyandrew.net>
2017-10-22 21:31:00 +0530

    ist

--- app.rb
+++ app.rb
@@ -10,12 +10,16 @@ set :port, 9494
 
 BIRTH = DateTime.parse('2017-10-12T12:06:00+05:30')
 
+def now
+  DateTime.now.new_offset(11/48.to_f) # Force IST
+end
+
 def since_birth
-  Jekyll::Timeago::Core.timeago(BIRTH, DateTime.now, depth: 4)
+  Jekyll::Timeago::Core.timeago(BIRTH, now, depth: 4)
 end
 
 def summary
-  base = DateTime.now - BIRTH
+  base = now - BIRTH
   days = base.to_i
   hours = (base * 24).to_i
   minutes = (base * 24 * 60).to_i
